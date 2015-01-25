1d952a771542766376ee1591b28b6f6c10da5f4a
Gregory Bityukov <Gregory.Bityukov@gmail.com>
2015-01-25 04:05:45 +0300

    first steps

---
+++ src/main/java/ru/kamapcuc/myownwotreplays/Main.java
@@ -0,0 +1,66 @@
+package ru.kamapcuc.myownwotreplays;
+
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequest;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.node.Node;
+import org.elasticsearch.node.NodeBuilder;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+
+    private static Parser parser = new Parser();
+
+    public static AtomicInteger indexed;
+    public static AtomicInteger all;
+
+    public static void main(String[] args) {
+        ImmutableSettings.Builder settings = ImmutableSettings.settingsBuilder();
+        settings.loadFromClasspath("main/resources/elasticsearch.yml");
+        NodeBuilder nodebuilder = new NodeBuilder();
+        nodebuilder.settings(settings);
+        Node node = nodebuilder.node();
+        node.start();
+        Client client = node.client();
+        ClusterHealthRequest health = new ClusterHealthRequest();
+        health.waitForGreenStatus();
+        //   client.admin().cluster().health(health).actionGet();
+
+        Path replays = Paths.get("../replays");
+
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(replays)) {
+            for (Path entry : stream) {
+                if (!Files.isDirectory(entry) && entry.toString().endsWith(".wotreplay")) {
+
+                    try (BufferedReader reader = new BufferedReader(new FileReader(entry.toString()))) {
+                        Map<String, Object> test;
+                        String data = reader.readLine();
+                        if (data.length() == 8) {
+                            data = reader.readLine();
+                            test = parser.parse(data.substring(3));
+                        } else
+                            test = parser.parse(data.substring(12));
+
+                        if (test == null)
+                            System.out.println(String.format("Failed to parse %s", entry.toFile().getName()));
+
+                    }
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        System.out.println(Arrays.toString(parser.x));
+    }
+}
---
+++ src/main/java/ru/kamapcuc/myownwotreplays/Parser.java
@@ -0,0 +1,41 @@
+package ru.kamapcuc.myownwotreplays;
+
+import org.codehaus.jackson.map.ObjectMapper;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class Parser {
+
+    private final String RX = "\u0000\u0000";
+    private final ObjectMapper mapper = new ObjectMapper();
+//    public int[] x = new int[]{0,0,0,0};
+
+    public Map<String, Object> parse(String data) {
+        Map<String, Object> result = null;
+        String[] parts = data.split(RX);
+        switch (parts.length) {
+            case 3:
+            case 2:
+                String battleFinishInfo = parts[1];
+                if (battleFinishInfo != null)
+                    try {
+                        mapper.readValue(battleFinishInfo, List.class);
+                    } catch (IOException ignored) {
+                    }
+            case 1:
+                String battleStartInfo = parts[0];
+                if (battleStartInfo != null)
+                    try {
+                        result = mapper.readValue(battleStartInfo, Map.class);
+                    } catch (IOException ignored) {
+                    }
+        }
+//        x[parts.length]++;
+//        if (result == null)
+//            System.out.println(String.format("Failed to parse %s", data));
+        return result;
+    }
+
+}
