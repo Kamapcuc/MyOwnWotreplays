f90ba8b138ffee254f187eef15649177472e74ec
Gregory Bityukov <Gregory.Bityukov@gmail.com>
2015-01-27 14:55:34 +0300

    draft 2

---
+++ src/main/java/ru/kamapcuc/myownwotreplays/elastic/DataConfig.java
@@ -0,0 +1,14 @@
+package ru.kamapcuc.myownwotreplays.elastic;
+
+public class DataConfig {
+
+    public final static String REPLAYS_INDEX_NAME = "replays";
+    public final static String BATTLE_TYPE_NAME = "battle";
+
+    public final static String DATA_INDEX_NAME = "data";
+    public final static String TANK_TYPE_NAME = "tank";
+    public final static String MAP_TYPE_NAME = "map";
+    public final static String CLASS_TYPE_NAME = "class";
+    public final static String NATION_TYPE_NAME = "nation";
+
+}
