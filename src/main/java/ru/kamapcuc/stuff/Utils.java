68676fdfd02be2f7cc9c85c7feeecee66d3a024c
Gregory Bityukov <Gregory.Bityukov@gmail.com>
2015-02-01 01:33:49 +0300

    small fixes

--- src/main/java/ru/kamapcuc/stuff/Utils.java
+++ src/main/java/ru/kamapcuc/stuff/Utils.java
@@ -3,8 +3,8 @@ package ru.kamapcuc.stuff;
 
 public class Utils {
 
-    public static <T> T coalesce(T... items) {
-        for (T i : items)
+    public static Object coalesce(Object... items) {
+        for (Object i : items)
             if (i != null)
                 return i;
         return null;
